436a64179f86e1074800f19992e1bc3666f086f3
Fgrimaldos <165615070+Fgrimaldos@users.noreply.github.com>
2025-07-07 00:38:26 -0500

    cambio main para aceptar css y js

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from flask_cors import CORS
 import json
 import os
 from datetime import datetime
-from os import getenv  # ← necesario para Railway
+from os import getenv  # Necesario para Railway
 
 app = Flask(__name__)
 CORS(app)
 
 GPS_FILE = "data.json"
 
-# Crear archivo vacío si no existe
+# Crear archivo si no existe
 if not os.path.exists(GPS_FILE):
     with open(GPS_FILE, "w") as f:
         json.dump([], f)
@@ -19,7 +19,7 @@ if not os.path.exists(GPS_FILE):
 def recibir_datos():
     try:
         data = request.get_json()
-        print("📥 Datos recibidos:", data)
+        print("Datos recibidos:", data)
 
         nuevo_dato = {
             "device_id": data.get("device_id", ""),
@@ -35,27 +35,28 @@ def recibir_datos():
             json.dump(contenido, f, indent=2)
 
         return jsonify({"status": "ok"}), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-@app.route("/api/clear", methods=["POST"])
-def borrar_datos():
-    try:
-        with open(GPS_FILE, "w") as f:
-            json.dump([], f)
-        print("🗑️ Datos GPS eliminados.")
-        return jsonify({"status": "datos eliminados"}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 @app.route("/data.json")
 def obtener_datos():
-    return send_from_directory('.', GPS_FILE)
+    return send_from_directory(".", GPS_FILE)
 
 @app.route("/")
 def index():
-    return send_from_directory('.', "mapa.html")
+    return send_from_directory(".", "mapa.html")
+
+@app.route("/estilos.css")
+def estilos():
+    return send_from_directory(".", "estilos.css")
+
+@app.route("/mapa.js")
+def script():
+    return send_from_directory(".", "mapa.js")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(getenv("PORT", 5000)))
+
 
